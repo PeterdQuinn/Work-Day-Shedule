ac1ec4fdb71bc80b5915240a6dc0ea7bed3fce7b
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-03-26 01:42:10 -0700

    master

--- Develop/index.js
+++ Develop/index.js
@@ -1,89 +1,49 @@
-today = new Date();
-currentMonth = today.getMonth();
-currentYear = today.getFullYear();
-selectYear = document.getElementById("year");
-selectMonth = document.getElementById("month");
 
-months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+// GLOBAL VARIABLES
+// display current time/date in header
+$("#currentDay").append(moment().format("LLLL"));
 
-monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
+// set current date/time for reference in blocks
+let currentTime = moment().hour();
 
+let saveEntry = "";
 
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
-}
+// FUNCTIONS
+// recall entries from local storage
+$(".time-block").each(function() {
+  let timeEntryId = $(this).find(".hour").attr("id");
+  let timeDesc = localStorage.getItem(timeEntryId);
+  $(this).find(".description").val(timeDesc);
 
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
+  var refTime = $(this).find(".description").attr("data-hour");
 
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
-function showCalendar(month, year) {
-
-    let firstDay = (new Date(year, month)).getDay();
-
-    tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-
-    // creating all cells
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                cell = document.createElement("td");
-                cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }
-            else if (date > daysInMonth(month, year)) {
-                break;
-            }
-
-            else {
-                cell = document.createElement("td");
-                cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
-                } // color today's date
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                date++;
-            }
+  // for each description class, run this function to color-coordinate blocks
+  if (refTime > currentTime) {
+    $(this).find(".description").addClass("future");
+  } else if (refTime == currentTime) {
+    $(this).find(".description").addClass("present");
+  } else if (refTime < currentTime) {
+    $(this).find(".description").addClass("past");
+  };
+});
 
 
-        }
+// save entry to local storage
+$(".saveBtn").on("click", function() {
+  // console.log("click");
+  
+  saveEntry = $(this)
+    .siblings(".description")
+    .val();
+  // console.log(saveEntry);
 
-        tbl.appendChild(row); // appending each row into calendar body.
-    }
+  let timeEntry = $(this)
+    .siblings(".hour")
+    .attr("id");
+    
+  localStorage.setItem(timeEntry, saveEntry);
+});
 
-}
-
-
-// check how many days in a month code from https://dzone.com/articles/determining-number-days-month
-function daysInMonth(iMonth, iYear) {
-    return 32 - new Date(iYear, iMonth, 32).getDate();
-}
 
 
 
